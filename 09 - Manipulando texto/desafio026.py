4dad24c1ad78cafd6e716e16bc4b3484e646ab2e
andrefalken <76755107+andrefalken@users.noreply.github.com>
2020-12-29 22:40:05 -0300

    Update desafio026.py

--- 09 - Manipulando texto/desafio026.py
+++ 09 - Manipulando texto/desafio026.py
@@ -2,12 +2,13 @@
 # Faça um programa que leia uma frase pelo teclado e mostre:
 
 # Solicitando ao(a) usuário(a) que digite a frase
-frase = input('Digite a frase: ')
+frase = str(input('Digite uma frase: ')).upper().strip()
 
 # Quantas vezes aparece a letra a minúscula
-print('A letra a minúscula aparece ', frase.count('a'), 'vezes na frase: {}'.format(frase))
+print('A letra A maiúscula aparece {} vezes na frase: {}.'.format(frase.count('A'), frase))
 
 # Em que posição a letra a aparece pela primeira vez
-print('A letra a minúscula aparece a primeira vez no índice ', frase.find('a'), 'na frase: {}'.format(frase))
+print('A letra A maiúscula aparece a primeira vez no índice {} na frase: {}.'.format(frase.find('A'), (frase)))
 
 # Em que posição ela aparecer pela última vez
+print('A letra A maiúscula aparece a última vez no índice {} na frase: {}.'.format(frase.rfind('A'), (frase)))
